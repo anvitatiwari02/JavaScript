812e0e3d2a5fbd8e0a3cedcfd29c9024fd0e6353
Anvita Tiwari <anvitatiwari02@gmail.com>
2014-11-14 13:02:08 +0530

    Create Introductionto'While'LoopsinJS.js

---
+++ Introductionto'While'LoopsinJS.js
@@ -0,0 +1,115 @@
+var coinFace = Math.floor(Math.random() * 2);
+
+while(coinFace === 0){
+	console.log("Heads! Flipping again...");
+	var coinFace = Math.floor(Math.random() * 2);
+}
+console.log("Tails! Done flipping.");
+
+
+var understand = true;
+
+while(understand)
+{
+	console.log("I'm learning while loops!");
+	understand = false;
+}
+
+
+understand = true;
+
+while(understand){
+	console.log("I'm learning while loops!");
+	//Change the value of 'understand' here!
+	understand = false;
+	
+}
+
+
+var bool = true;
+
+while(bool){
+    console.log("Less is more!");
+    bool = false;
+}
+
+
+//Remember to set your condition outside the loop!
+
+
+var loop = function(){
+    var number = 0;
+    while(number<3){
+        console.log("I'm looping!");
+        number++;
+    }
+};
+loop();
+
+
+//Remember to make your condition true outside the loop!
+
+var soloLoop = function(){
+  var bool = true;
+  while(bool)
+  {
+      console.log("Looped once!");
+      bool = false;
+  }
+};    
+soloLoop();
+
+
+
+ var loops = function (){
+
+    var counter = 0;
+    while(counter ++ < 3)
+    {    
+    console.log("Do Something!");
+    }
+};
+
+counter = true;
+
+for (var i = 3; i === 5; i--);
+{
+console.log(i);
+}
+i = false;
+
+
+var loopCondition = false;
+
+do {
+	console.log("I'm gonna stop looping 'cause my condition is " + loopCondition + "!");	
+} while (loopCondition);
+
+
+var getToDaChoppa = function(){
+// Write your do/while loop here!
+do {
+console.log("u r very sweet");
+}
+while(getToDaChoppa = false);
+};
+getToDaChoppa();
+
+
+// Write your code below!
+for(var i=1;i<=10;i++)
+{
+    console.log(i);
+}
+var bool=true;
+while(bool)
+{
+    console.log("shdfgsdghfjsdhg");
+    bool=false;
+}
+var booll=false;
+do
+{
+    console.log("fhgfhghg");
+  
+}while(booll);
