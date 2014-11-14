341d2403a15bfed4cc1b2041483355c07134aa63
Anvita Tiwari <anvitatiwari02@gmail.com>
2014-11-14 12:59:08 +0530

    Create Introductionto'For'LoopsinJS.js

---
+++ Introductionto'For'LoopsinJS.js
@@ -0,0 +1,85 @@
+// Write five console.log statements.
+console.log(1);
+console.log(2);
+console.log(3);
+console.log(4);
+console.log(5);
+
+// Example of a for loop:
+
+for (var counter = 1; counter < 11; counter++) {
+	console.log(counter);
+}
+
+
+// Change where the for loop starts.
+
+for (var i = 5; i < 11; i = i + 1){
+	console.log(i);
+}
+
+
+// Edit this for loop
+
+for (var i = 4; i <=23; i = i + 1) {
+	console.log(i);
+}
+
+
+// Edit this for loop!
+
+for (var i = 5; i <51; i=i+5) {
+	console.log(i);
+}
+
+
+// Example for loop
+
+for (var i = 8 ; i < 120; i=i+12) {
+	console.log(i);
+}
+
+
+// Example of infinite loop. THIS WILL CRASH YOUR
+// BROWSER. Don't run the code without changing it!
+
+for (var i = 10; i >= 0; i--) {
+	console.log(i);
+}
+
+
+// Write your very own for loop!
+for(var i=100;i>0;i=i-5)
+{
+    console.log(i);
+}
+
+
+// You are now declaring an array.
+// Arrays are an awesome data structure! 
+var junk=["anvi","dalip",2,1];
+console.log(junk);
+
+
+// Practice array!
+
+var junkData = ["Eddie Murphy", 49, "peanuts", 31];
+
+console.log(junkData[3]);
+
+
+// Let's print out every element of an array using a for loop
+
+var cities = ["Melbourne", "Helsinki", "NYC"];
+
+for (var i = 0; i < cities.length; i++) {
+    console.log("I would like to visit " + cities[i]);
+}
+
+
+// Click on "Stuck? Get a hint!" if you get stuck!
+var names=["anvi","dalip","aaaa","ssss","dddd"];
+for(var i=0;i<names.length;i++)
+console.log("I know someone called "+ names[i]);
+
+
