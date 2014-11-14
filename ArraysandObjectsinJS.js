f58e38dcc95b1c4a0878d11023403325f35f849c
Anvita Tiwari <anvitatiwari02@gmail.com>
2014-11-14 13:07:44 +0530

    Create ArraysandObjectsinJS.js

---
+++ ArraysandObjectsinJS.js
@@ -0,0 +1,100 @@
+var list=["sgdg","twe",1,22,33];
+
+
+
+var languages = ["HTML", "CSS", "JavaScript", "Python", "Ruby"];
+{
+console.log(languages[2]);
+}
+
+
+
+var languages = ["HTML", "CSS", "JavaScript", "Python", "Ruby"];
+{
+console.log(languages[2]);
+console.log(languages.length);
+}
+
+
+
+var languages = ["HTML", "CSS", "JavaScript", "Python", "Ruby"];
+for(var i=0;i < languages.length;i++)
+{
+    console.log(languages[i]);
+}
+
+
+
+var myArray=[23,true,"anvi"];
+
+
+
+var newArray=[[1,1,1],[23,12,12],[14,23,13]];
+
+
+
+var jagged=[[23],[1,1],[12,23,34]];
+
+
+
+var phonebookEntry = {};
+
+phonebookEntry.name = 'Oxnard Montalvo';
+phonebookEntry.number = '(555) 555-5555';
+phonebookEntry.phone = function() {
+  console.log('Calling ' + this.name + ' at ' + this.number + '...');
+};
+
+phonebookEntry.phone();
+
+
+var me={
+    name:"anvi",
+age:23
+};
+
+
+
+
+var me=new Object();
+me.name="anvi";
+me.age=23;
+
+
+var object1=new Object()
+object1.name1="hdsfg"
+var object2=new Object()
+object2.name2="hdsfg33ss"
+var object3=new Object()
+object3.name2="hdsfg122"
+
+
+
+var myArray=[23,true,"anvi",[myobj]];
+var myobj=new Object()
+myobj.name="anvi1111";
+
+
+
+var newArray=[[1],[3,[my]],[1,3,5]];
+var my=new Object();
+my.name="anvi";
+
+
+
+var myObject = {
+  name: 'Eduardo',
+  type: 'Most excellent',
+   interests:[23,233]
+  // Add your code here!
+  
+};
+
+
+
+
+var myOwnObject=new Object();
+myOwnObject.name="asas";
+myOwnObject.age="23";
+
+
